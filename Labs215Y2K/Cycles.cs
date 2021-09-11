da45138aac6d344062d553f1fc8bb81bf4278674
r3dinc <r3dinc@yandex.ru>
2021-09-11 12:53:35 +0300

    start commit cycles work

---
+++ Labs215Y2K/Cycles.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class Cycles
+    {
+        static void StepenChisla()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static void OdinakoviyCifer()
+        {
+            Console.WriteLine(";oreighposdihgd");
+        }
+
+    }
+}
