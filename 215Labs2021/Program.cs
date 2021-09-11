661c9364af1821d83d182bf23ee3de403c1aec02
r3dinc <r3dinc@yandex.ru>
2021-09-11 12:24:43 +0300

    create project commit

---
+++ 215Labs2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _215Labs2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
