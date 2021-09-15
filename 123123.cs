c93fdbf52dc8c2b716de4a31012c4b967b5abe5a
KUDOSOV <90503837+Kuch86@users.noreply.github.com>
2021-09-15 10:43:15 +0300

    Add files via upload

---
+++ 123123.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace bot2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wassup! Pls tell what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Nice to meet you,",name);
+            Console.WriteLine($"{name}, how old are you:?");
+            int num = int.Parse(Console.ReadLine());
+            
+      
+        }
+    }
+}
