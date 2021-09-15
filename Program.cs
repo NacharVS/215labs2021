8be629ea3a276c87a277305759e6859fff4adea5
marcelomrv <90503719+marcelomrv@users.noreply.github.com>
2021-09-15 10:34:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp12212
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int chislo = 0;
+            while (a > 0)
+            {
+                chislo *= 10;
+                chislo += a % 10;
+                a /= 10;
+            }
+
+            Console.WriteLine($"{chislo}");
+        }
+    }
+}
