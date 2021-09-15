8356b0c43448dc84075c453213a8b4b6fd287fb3
Nikiforov <90503746+guala228@users.noreply.github.com>
2021-09-15 10:22:50 +0300

    Add files via upload

---
+++ Program (1).cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace bot2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Привет! как тебя зовут!");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Рад знакмоству, {name}");
+            Console.WriteLine($"{name}, Сколько тебе лет?");
+            int num = int.Parse(Console.ReadLine());
+            if (num>18.23)
+                Console.WriteLine("Тебе больше 18");
+            else if (num<10)
+                Console.WriteLine("Ты ещё очень маленький");
+            else 
+                Console.WriteLine("Ты ещё маленький");
+            
+            
+
+        }
+    }
+}
