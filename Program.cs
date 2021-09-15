d1dc4194e311becb7a7510994d11c6428e62eaa7
marcelomrv <90503719+marcelomrv@users.noreply.github.com>
2021-09-15 10:10:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Stepen__chisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число 1");
+            long a = long.Parse(Console.ReadLine());
+            Console.WriteLine("Введие число 2");
+            long b = long.Parse(Console.ReadLine());
+            long c = 1;
+            for (int i = 1; i <= b; i++)
+            {
+                c = a * c;
+            }
+            Console.WriteLine($"Число равно {c}");
+        }
+    }
+}
