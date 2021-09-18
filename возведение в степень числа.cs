0ccec48de44a2f98dda9a536e0e06395ec6e2e83
bulataboba <90503889+bulataboba@users.noreply.github.com>
2021-09-18 12:11:52 +0300

    Add files via upload

---
+++ возведение в степень числа.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введие второе число");
+
+            int b = int.Parse(Console.ReadLine());
+
+            int c = 1;
+
+            for (int i = 1; i <= b; i++)
+            {
+                c = a * c;
+            }
+            Console.WriteLine($"Число {a} в {b} степени равно  {c}");
+        }
+    }
+}
