45ef54c5632324dfd7b4e340f288689b8c341ab2
bulataboba <90503889+bulataboba@users.noreply.github.com>
2021-09-18 12:18:26 +0300

    Add files via upload

---
+++ переворот числа.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace переворот_числа
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, которое хотите перевернуть:");
+            int a = int.Parse(Console.ReadLine());
+            int b = 0;
+            while (a > 0)
+            {
+                b *= 10;
+                b += a % 10;
+                a /= 10;
+            }
+
+            Console.WriteLine($"Перевернутое число:  {b}");
+        }
+    }
+}
