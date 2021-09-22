8091f4b7cb6c6bcd568c7e8e041491da6cd8b716
abdullin123 <90503960+abdullin123@users.noreply.github.com>
2021-09-22 07:42:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace abdullin3
+{
+   
+class Program
+        {
+            static void Main(string[] args)
+            {
+                int number, result = 0;
+
+                Console.Write("enter number: ");
+                number = Int32.Parse(Console.ReadLine());
+
+                while (number > 0)
+                {
+                    result *= 10;
+                    result += number % 10;
+                    number /= 10;
+                }
+
+                Console.WriteLine(result);
+
+            }
+        }
+    
+}
