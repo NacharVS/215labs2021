25a2ceebc9c700fa36a65ef103317fa07282ee75
marcelomrv <90503719+marcelomrv@users.noreply.github.com>
2021-09-22 11:09:01 +0300

    Add files via upload

---
+++ 1 zadanie false.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                array[i] = new Random().Next(10, 100);
+                Console.Write($" {array[i]}");
+            }
+            int min = array[0];
+            int max = min;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+                Console.WriteLine(min);
+                Console.WriteLine(max);
+            }
+   
+            
+        }
+    }
+}
