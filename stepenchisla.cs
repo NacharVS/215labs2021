288922342bf5bb4f6d5a185aba32629fc3ec2b35
abdullin123 <90503960+abdullin123@users.noreply.github.com>
2021-09-22 07:53:18 +0300

    Add files via upload

---
+++ stepenchisla.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Console.WriteLine("число:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("степень:");
+            int b = int.Parse(Console.ReadLine());
+            int c = 1; 
+            for (int i = 1; i <= b; i++)
+            {
+                c = a * c;
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
+
+
+
+
