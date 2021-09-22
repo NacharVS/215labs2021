64ef39da6eb144639bb2350d7b51021ddbea26b5
r3dinc <r3dinc@yandex.ru>
2021-09-22 10:28:50 +0300

    array op. add

---
+++ Labs215Y2K/ArrayOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Labs215Y2K
+{
+    class ArrayOperations
+    {
+        static void OperationOne()
+        {
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(10, 100);
+
+                Console.Write($" {array[i]}");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] < array[i + 1])
+                {
+                    Console.Write($" {array[i + 1]}");
+                }
+            }
+        }
+    }
+}
