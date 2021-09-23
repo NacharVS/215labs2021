ec4a84cb8d08984d1cc12001fa1be13eabf5e686
abdullin123 <90503960+abdullin123@users.noreply.github.com>
2021-09-23 11:08:29 +0300

    Add files via upload

---
+++ Labs215Y2K/min i max.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = new Random().Next(10, 100);
+            int min, max;
+            min = array[0];
+            max = array[0];
+            for (int i = 1; i < array.Length; i++)
+                
+            {
+                if (array[i] < min)
+                    min = array[i];
+                if (array[i] > max)
+                    max = array[i];
+            }
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+        }
+    }
+}
