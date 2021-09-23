aabd0b2e7229c28e2f6c8361f6e6a9e5006f22ab
bulatboba <safikovbulan6@gmail.com>
2021-09-23 23:31:59 +0300

    ArrayEx

---
+++ Labs215Y2K/ArrayEx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class ArrayEx
+    {
+        public static void Ex3()
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(0, 100);
+                Console.Write($" {array[i]}");
+            }
+            Console.WriteLine("");
+            int[] b = new int[10];
+            for (int j = 0; j < b.Length; j++)
+            {
+                b[j] = new Random().Next(0, 100);
+                Console.Write($" {b[j]}");
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < b.Length; j++)
+                    if (array[i] == b[j])
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine($"Общее число: {array[i]}");
+                        break;
+                    }
+
+            }
+
+        }
+        public static void Ex5()
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(0, 100);
+                Console.Write($" {array[i]}");
+            }
+            int min = array[0];
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+
+
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"Минимальное значение: {min}");
+            Console.WriteLine("");
+            Console.WriteLine($"Максимальное значение: {max} ");
+
+        }
+    }
+}
