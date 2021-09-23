c4c4f4e12213235f82e1221036c2335fcdeee477
Artyom <artyom-void555@mail.ru>
2021-09-23 22:27:48 +0300

    HomeWork

---
+++ Labs215Y2K/sumproduct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class sumproduct
+    {
+        static void arr2()
+        {
+            int[] array1 = new int[10];
+            int[] array2 = new int[10];
+            int sum;
+            int pr;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = new Random().Next(10, 100);
+                array2[i] = new Random().Next(10, 100);
+                Console.Write(array1 + " ");
+                Console.WriteLine();
+                Console.WriteLine(array2 + " ");
+            }
+            for (int i = 0; i < array1.Length; i++)
+            {
+                sum = array1[i] + array2[i];
+                pr = array1[i] * array2[i];
+                Console.Write($"Сумма {sum} ");
+                Console.WriteLine($"Произведение {pr}");
+            }
+        }
+    }
+}
