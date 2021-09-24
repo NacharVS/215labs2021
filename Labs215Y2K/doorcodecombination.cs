e22f1ad74950762f3f6628c4f82b4b2532a54250
marcelomrv <kuzyakovmarsel@gmail.com>
2021-09-25 00:48:06 +0300

    bot

---
+++ Labs215Y2K/doorcodecombination.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class doorcodecombination
+    {
+        public static void ex1()
+        {
+            int a = int.Parse(Console.ReadLine());
+            for (int x1 = 0; x1 <= 9; x1++)
+            {
+                for (int x2 = x1 + 1; x2 <= 9; x2++)
+                {
+                    for (int x3 = x2 + 1; x3 <= 9; x3++)
+                    {
+                        if ((x1 + x2 + x3 == a) && (x1 != x2) && (x1 != x3) && (x2 != x3))
+                        {
+                            Console.Write(x1);
+                            Console.Write(x2);
+                            Console.WriteLine(x3);
+                        }
+                        else Console.Write("");
+                    }
+                }
+            }
+        }
+    }
+}
