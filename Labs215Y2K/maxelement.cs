97bd76a8f5ab2271dfaeb013702d53ed05128a1d
nikita262001 <nikita262001@mail.ru>
2021-09-24 16:48:57 +0300

    qq

---
+++ Labs215Y2K/maxelement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ArrayOperationsAbd
+{
+    class maxelement
+    {
+        public static void maxOperations()
+        {
+            int[,] array = new int[10, 10];
+
+            int max = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
