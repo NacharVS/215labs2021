c5b2c77763c17d5d9c3c94dc0a2fc45cbc297975
foxenberg <daniel.korshunov.2001@gmail.com>
2021-09-24 16:31:58 +0300

    1

---
+++ Labs215Y2K/2dArray.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class _2dArray
+    {
+        static void ex1()
+        {
+
+        }
+    }
+}
