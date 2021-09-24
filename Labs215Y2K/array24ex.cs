e9a0033cad6388590ca4439e88ae24fcb07ac812
marcelomrv <kuzyakovmarsel@gmail.com>
2021-09-24 23:37:33 +0300

    mavrova kuzyakov 3 ex

--- Labs215Y2K/array24ex.cs
+++ Labs215Y2K/array24ex.cs
@@ -6,27 +6,44 @@ namespace Labs215Y2K
 {
     class array24ex
     {
-        public static void ex1()
+        public static void ex3()
         {
             int[,] array = new int[10, 10];
-
-            int summ = 0;
-
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    array[i, j] = new Random().Next(0, 3);
-                    summ += array[i, j];
+                    array[i, j] = new Random().Next(1, 10);
                     Console.Write($" {array[i, j]}");
                 }
+                Console.WriteLine("   ");
+            }
+            Console.WriteLine("");
+            int[,] array1 = new int[10, 10];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array1[i, j] = new Random().Next(1, 10);
+                    Console.Write($" {array1[i, j]}");
+                }
+                Console.WriteLine("   ");
+
+            }
+            Console.WriteLine("");
+            int[,] array2 = new int[10, 10];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array2[i, j] = array[i, j] * array1[i, j];
+                    Console.Write($" {array2[i, j]}");
+                }
                 Console.WriteLine("");
-
-
-
-
-
-            }    
+            }
+            
         }
     }
 }
+    
+
