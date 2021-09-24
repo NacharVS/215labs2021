71b2ab6a7f3972d920292ad2d145fac9e2432f4f
marcelomrv <kuzyakovmarsel@gmail.com>
2021-09-24 20:40:26 +0300

    start

---
+++ Labs215Y2K/array24ex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class array24ex
+    {
+        public static void ex1()
+        {
+            int[,] array = new int[10, 10];
+
+            int summ = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(0, 3);
+                    summ += array[i, j];
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.WriteLine("");
+
+
+
+
+
+            }    
+        }
+    }
+}
