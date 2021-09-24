da4cf2b3b406bceb519cdbf6b51c5ef8bbccfe37
marcelomrv <kuzyakovmarsel@gmail.com>
2021-09-25 00:36:11 +0300

    1 ex

--- Labs215Y2K/array24ex.cs
+++ Labs215Y2K/array24ex.cs
@@ -41,7 +41,36 @@ namespace Labs215Y2K
                 }
                 Console.WriteLine("");
             }
-            
+        }
+        public static void ex1()
+        {
+            int[,] array = new int[10, 10];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(1, 100);
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.WriteLine();
+            }
+
+            int max = array[0, 0];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+
+                    if (max < array[i,j])
+                    {
+                        max = array[i, j];
+                    }
+                }
+                Console.Write($" Наибольший элемент строки {max}");
+                max = 0;
+                Console.WriteLine();
+            }
         }
     }
 }
