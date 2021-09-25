852982317b2f2cdf89d43e32bf56edd9a2e07d6e
abdullin123 <abdullinktits@gmail.com>
2021-09-25 10:08:15 +0300

    qq

--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -4,9 +4,27 @@ namespace Labs215Y2K
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main1(string[] args)
         {
-                  
+            Console.WriteLine("Введите размерность массива");
+            int n, m;
+            n = Convert.ToInt32(Console.ReadLine());
+            m = Convert.ToInt32(Console.ReadLine());
+            int[,] a = new int[m, n];
+            Random r = new Random();
+            for (int i = 0; i < m; i++)
+            {
+                int max = a[i, 0];
+                for (int j = 0; j < n; j++)
+                {
+                    if (a[i, j] > max)
+                        max = a[i, j];
+
+                }
+                Console.WriteLine("max:{0}", max);
+            }
+
         }
     }
+    
 }
