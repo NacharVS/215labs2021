069ed63bfde9a38bb87f8f16a05326af1f905cee
marcelomarv <kuzyakovmarsel@gmail.com>
2021-09-25 12:52:04 +0300

    start

---
+++ Labs215Y2K/arrayslesson25.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class arrayslesson25
+    {
+        public static void ex1()
+        {
+
+        }
+    }
+}
