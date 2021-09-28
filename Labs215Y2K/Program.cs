fcda957ba3e1260d5c017268e40ceea7a70012db
Subbot215 <egor-e002@mail.ru>
2021-09-28 15:31:17 +0300

    SwapArray

--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -7,10 +7,12 @@ namespace Deletor
     {
         public static void Main()
         {
-            //MaxMin.Maxmin.ex1();
-            //Multplus.MultPlus.ex2();
-            //MaxElement.MaxElem.ex3();
-            SumMatrix.SumMatrix.ex4();
+            //MaxMin.Maxmin.ex1(); // - максимальный элемент массива
+            //Multplus.MultPlus.ex2();  // - Сумма и умножение массива
+            //MaxElement.MaxElem.ex3(); // - Максимальный элемент в каждой строке
+            //SumMatrix.SumMatrix.ex4(); // - Сумма каждой строки матрицы
+            SwapArray.Swap.ex5();
+
             Console.WriteLine();
         }
     }
---
+++ Labs215Y2K/SwapArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SwapArray
+{
+    public class Swap
+    {
+        public static void ex5()
+        {
+            int[,] array = new int[2, 7];
+            int swap = array[0, 0];
+            int k = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(0, 10);
+                    Console.Write($" {array[i, j]}");
+                }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            
+         for (int i = 1; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($" {array[i, j]}");
+                    if (k != array.GetLength(1))
+                    {
+                        swap = array[0,j];
+                        k++;
+                        Console.Write($" {swap}");
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
