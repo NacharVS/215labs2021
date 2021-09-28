f12b75cf392b8b8530faff5351d223dab7e33c7e
Gazhos215 <9253924@mail.ru>
2021-09-28 15:33:51 +0300

    1

---
+++ Labs215Y2K/Диагональ матрицы.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Diagonal
+{
+    class Diagonal
+    {
+       public static void ex2(string[] args)
+        {
+            int[,] array = new int[10, 10];
+
+            int summ = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(0, 2);
+                    summ += array[i, j];
+
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.Write($"     {summ}");
+                summ = 0;
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum += array[i, i];
+                
+            }
+            Console.WriteLine($"Сумма главной диагонали {sum}");
+        }
+    }
+}
