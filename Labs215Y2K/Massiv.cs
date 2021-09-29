21c2d26d330a23cff41c46eda18e1b5cc711a314
F1aris <kflars2004@mail.ru>
2021-09-29 19:16:01 +0300

    New commit

---
+++ Labs215Y2K/Massiv.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class Massiv
+    {
+        public static void ex1()
+        {
+            int[] mas = new int[10];
+            Random r = new Random();
+            for (int i = 0; i < mas.Length; i++)
+                mas[i] = r.Next(0, 100);
+            for (int i = 0; i < mas.Length; i++)
+                Console.Write(mas[i] + " ");
+            Console.WriteLine();
+            int min, max;
+            min = max = mas[0];
+            for (int i = 1; i < mas.Length; i++)
+            {
+                if (min > mas[i])
+                {
+                    min = mas[i];
+                }
+                if (max < mas[i])
+                {
+                    max = mas[i];
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Макс {max}");
+            Console.WriteLine($"Мин {min}");
+
+        }
+          
+        
+            
+
+    }
+}
