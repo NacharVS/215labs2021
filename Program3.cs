0ff4abed35b45131dcf5cca3b7378ffddb478bdd
kamilyamm <90504087+kamilyamm@users.noreply.github.com>
2021-09-29 22:17:57 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Consoleapp
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("число:");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("степень:");
+                int b = int.Parse(Console.ReadLine());
+                int c = 1;
+                for (int i = 1; i <= b; i++)
+                {
+                    c = a * c;
+                }
+            Console.WriteLine(c);
+            }
+        }
+    }
+
+
+
