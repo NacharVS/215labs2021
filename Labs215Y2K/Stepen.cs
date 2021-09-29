0a67e753555351e64e1276a0b990da3f4150d881
Alfrdedo <aimbot727@gmail.com>
2021-09-29 21:55:22 +0300

    Степень числа

---
+++ Labs215Y2K/Stepen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class Stepen
+    {
+        public static void Ex1() 
+        {
+            Console.WriteLine("Введите число");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите степень числа");
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= x; i++)
+            {
+                num *= num;
+            }
+            Console.WriteLine(num);
+        }
+    }
+}
