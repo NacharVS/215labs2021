2f21701f0caa652230032b1e92e2ff58c73c5b04
Alfrdedo <aimbot727@gmail.com>
2021-09-29 23:16:23 +0300

    Min

---
+++ Labs215Y2K/MaxMin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class MaxMin
+    {
+        public static void Ex2() 
+        {
+            int[] Myarray = new int[10];
+            for (int i = 0; i < Myarray.Length; i++)
+            {
+                Myarray[i] = new Random().Next(0, 100);
+                Console.Write($" {Myarray[i]}");
+            }
+            Console.WriteLine();
+            Console.Write("для поиска минимального элемента нажмите ENTER:");
+            Console.ReadLine();
+            int min = Myarray[0];
+
+            for (int i = 0; i < Myarray.Length; i++)
+            {
+                if (min > Myarray[i])
+                    min = Myarray[i];
+            }
+            Console.WriteLine("___________________________________________________________________________");
+            Console.WriteLine("минимальный элемент:" + min);
+            Console.ReadLine();
+        }
+    }
+}
--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -6,7 +6,7 @@ namespace Labs215Y2K
     {
         static void Main(string[] args)
         {
-            Stepen.Ex1();
+            MaxMin.Ex2();
         }
     }
 }
