26add658216c5f687710cb436aa435102e1a6442
r3dinc <r3dinc@yandex.ru>
2021-09-29 11:10:02 +0300

    methods start

--- Labs215Y2K/ArrayOperations.cs
+++ Labs215Y2K/ArrayOperations.cs
@@ -7,28 +7,17 @@ namespace Labs215Y2K
         // 1. Найти сумму эл-ов в каждой строке матрицы
         // 2. Поменять местами  две строки матрицы
 
-        static void HomeWork()
-        {
-            int[,] array1 = new int[10, 10];
-            int sum = 0;
-
-            for (int i = 0; i < array1.GetLength(0); i++)
-            {
-                for (int j = 0; j < array1.GetLength(0); j++)
+        public static void TwoDimensionalArrayGeneration(int[,] array)
+        {         
+           for (int i = 0; i < array.GetLength(0); i++)
+           {
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    array1[i, j] = new Random().Next(0, 2);
-                    Console.Write($"{array1[i, j]} ");
+                    array[i, j] = new Random().Next(10, 100);
+                    Console.Write($"{array[i, j]} ");
                 }
                 Console.WriteLine();
-            }
-
-            for (int i = 0; i < array1.GetLength(0); i++)
-            {
-                sum += array1[i, i];
-            }
-
-            Console.WriteLine($" summ - {sum}");
-
+           }
         }
         static void TwoDimensionalArray()
         {
