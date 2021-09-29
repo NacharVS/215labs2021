70757f4b9be5fe920e0e8f66b7e414e16c56f13a
sal1wan <salopechkin@mail.ru>
2021-09-29 16:21:18 +0300

    new commit

--- Labs215Y2K/ArraysLast.cs
+++ Labs215Y2K/ArraysLast.cs
@@ -8,7 +8,33 @@ namespace Labs215Y2K
     {
         public static void Ex2() 
         {
-            Console.WriteLine("1");
+            Random x = new Random();
+            int[,] array = new int[3, 3];
+
+            for (int a = 0; a < array.GetLength(0); a++)
+            {
+                for (int b = 0; b < array.GetLength(1); b++)
+                {
+                    array[a, b] = x.Next(10, 100);
+                    Console.Write($"{ array[a, b]} ");
+
+                }
+                Console.WriteLine();
+            }
+
+            int sum = 0;
+
+            for (int a = 0; a < array.GetLength(0); a++)
+            {
+                for (int b = 0; b < array.GetLength(1); b++)
+                {
+                    sum += array[a, b];
+                }
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+            Console.ReadKey();
+            Console.WriteLine(sum);
         }
     }
 }
