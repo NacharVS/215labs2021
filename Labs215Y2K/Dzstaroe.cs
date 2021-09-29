fed331d85ce0d7f5c1bbd24ee5941ffcb1a88eba
bulatboba <safikovbulan6@gmail.com>
2021-09-29 16:08:48 +0300

    perevorot.

---
+++ Labs215Y2K/Dzstaroe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class Dzstaroe
+    {
+        public static void n1()
+        {
+            int number, result = 0;
+
+            Console.Write("enter number: ");
+            number = Int32.Parse(Console.ReadLine());
+
+            while (number > 0)
+            {
+                result *= 10;
+                result += number % 10;
+                number /= 10;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
