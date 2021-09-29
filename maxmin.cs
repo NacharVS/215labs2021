582dc2bd6e966980d47165ffbc0d8c53e93b87c6
SBG2K20 <90503601+SBG2K20@users.noreply.github.com>
2021-09-29 23:07:16 +0300

    Add files via upload

---
+++ maxmin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace maxmin
+{
+    class Program
+    {
+        static void maxmin( )
+        {
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(10, 100);
+                Console.Write($" {array[i]}");
+            }
+            int minvalue = array[0];
+            int maxvalue = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < minvalue)
+                {
+                    minvalue = array[i];
+                }
+                if (array[i] > maxvalue)
+                {
+                    maxvalue = array[i];
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine($" Миниммальное значение массива {minvalue}");
+            Console.WriteLine($" Максимальное значение массива {maxvalue}");
+        }
+    }
+}
