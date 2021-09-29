a6dc50909d136c4f4eaeedcdd008275705390068
kamilyamm <90504087+kamilyamm@users.noreply.github.com>
2021-09-29 21:38:01 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 
-namespace перевернутое_число
+namespace _22._09
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args, string sum)
         {
-            Console.WriteLine("введите трехзначное число,которое нужно перевернуть");
-            int n = Convert.ToInt32(Console.ReadLine());
-            n = (n / 100) + (n / 10 % 10 * 10) + (n % 10 * 100);
-            Console.WriteLine($" ваше перевернутое число - {n}");
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(10, 100);
+                {
+                    int[] numbers = new int[] { i };
+
+                    for (int i1 = 0; i1 < numbers.Length; i1++)
+                    {
+                        int value = numbers[i1];
+                        sum += value;
+                    }
+                }
+                Console.Write($" {array[i]}");
+                Console.WriteLine("Сумма элементов массива: " + sum);
+            }
         }
     }
 }
