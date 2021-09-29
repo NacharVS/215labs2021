da327080afd8305564b1f7fd24d9b72879e75dee
SBG2K20 <90503601+SBG2K20@users.noreply.github.com>
2021-09-29 22:25:27 +0300

    Add files via upload

---
+++ revers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Chislo_2
+{
+    class Program
+    {
+        static void reverse()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int chislo = 0;
+            while (a > 0)
+            {
+                chislo *= 10;
+                chislo += a % 10;
+                a /= 10;
+            }
+
+            Console.WriteLine($"{chislo}");
+        }
+    }
+}
+    
+
