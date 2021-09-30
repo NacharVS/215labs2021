cdc6e59fd6334659e024c9bb5dc71dbf0d2775b2
sal1wan <salopechkin@mail.ru>
2021-09-30 22:23:11 +0300

    Диогальная сумма матрицы

---
+++ Labs215Y2K/Diagonale.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class Diagonale
+    {
+        public static void Ex2() 
+        {
+            int[,] array = new int[3, 3];
+            int sum = 0;
+
+
+            Random z = new Random();
+            for (int a = 0; a < array.GetLength(0); a++)
+            {
+                for (int b = 0; b < array.GetLength(1); b++)
+                {
+                    array[a, b] = z.Next(100);
+                    Console.Write(array[a, b] + "\t");
+                }
+                Console.WriteLine();
+
+            }
+
+
+            for (int a = 0; a < array.GetLength(0); a++)
+            {
+
+                sum += array[a, a];
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+
+    }
+}
