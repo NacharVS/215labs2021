05d86b372cf7c48dd3b58f1b11728f08dd8d488c
YurovYan <yan.yurov.2005@mail.ru>
2021-09-30 14:25:04 +0300

    a в степени b

---
+++ Labs215Y2K/a в степени b.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class a_в_степени_b
+    {
+        public static void Ex1() 
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = a;
+            if (b == 0)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                for (int i = 2; i <= b; i++)
+                {
+                    a = a * c;
+                }
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
