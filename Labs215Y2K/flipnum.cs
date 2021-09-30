288f876e3dbf7aa2dc074da35cfe8b2717a5ad26
Gazhos215 <9352921@mail.ru>
2021-09-30 14:51:35 +0300

    30.09

---
+++ Labs215Y2K/flipnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace flip
+{
+    class flipnum
+    {
+        public static void ex7()
+        {
+            int result = 0;
+            Console.Write("Введите число: ");
+            int num = int.Parse(Console.ReadLine());
+
+            while (num > 0)
+            {
+                result = num % 10;
+                num = num / 10;
+                Console.Write(result);
+            }
+
+
+        }
+    }
+}
