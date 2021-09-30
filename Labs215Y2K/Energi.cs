b47e10f2fb7beb830940f667f34346314dc3cbca
F1aris <kflars2004@mail.ru>
2021-09-30 09:06:46 +0300

    New commit

---
+++ Labs215Y2K/Energi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class Energi
+    {
+        public static void ex7()
+        {
+            Console.WriteLine("Введите маасу и высоту");
+            int h = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int g = 10;
+            int F = h * m * g;
+            Console.WriteLine($"Энергия равна =" + F + "Н/кг");
+        }
+    }
+}
