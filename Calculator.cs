2f5e26f0ee3e26db71a859a64f1634b0d713bd16
SBG2K20 <90503601+SBG2K20@users.noreply.github.com>
2021-09-30 09:41:31 +0300

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Calsulator()
+        {
+            Console.WriteLine("Введите 2 числа:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите знак действия:");
+            string d =  Console.ReadLine();
+            if (d == "+")
+            {
+                Console.WriteLine($"Сумма:{a+b}");
+            }
+            if (d == "-")
+            {
+                Console.WriteLine($"Разность:{a - b}");
+            }
+             if (d == "/")
+             {
+                Console.WriteLine($"Чё то отделения:{a / b}");
+             }
+             if (d == "*")
+             {
+                Console.WriteLine($"Произведение:{a * b}");
+             }
+            
+        }
+    }
+}
