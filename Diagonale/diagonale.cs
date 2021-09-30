ca60823fe3b37496936a9ea05086ca9844c11480
Zarina666 <90504076+Zarina666@users.noreply.github.com>
2021-09-30 14:03:07 +0300

    Create diagonale.cs

---
+++ Diagonale/diagonale.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class Diagonal
+    {
+        static void main002(string[] args)
+        {
+            int R = 0, C = 0;
+            Console.WriteLine("Введите число строк и столбцов матрицы");
+            Console.Write("Число строк = ");
+            R = int.Parse(Console.ReadLine());
+            Console.Write("Число столбцов = ");
+            C = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            int[,] array = new int[R, C];
+            int sum = 0;
+            for (int r = 0; r < R; r++)
+            {
+                for (int c = 0; c < C; c++)
+                {
+                    array[r, c] = new Random().Next(0, 100);
+                }
+                if (r < R && r < C)
+                    sum += array[r, r];
+            }
+
+            Console.WriteLine("Создана матрица M[R,C] случайных чисел");
+            Console.WriteLine();
+            for (int r = 0; r < R; ++r)
+            {
+                for (int c = 0; c < C; ++c)
+                {
+                    Console.Write(array[r, c] + " \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Результат");
+            Console.WriteLine();
+            Console.WriteLine("Сумма диагонали матрицы = " + sum);
+            Console.ReadKey();
+        }
+    }
+}
