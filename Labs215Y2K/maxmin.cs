1ac354f27e01c5a64792cc0aad464f8c6b1e9f77
sal1wan <salopechkin@mail.ru>
2021-09-30 11:48:33 +0300

    MaxMin Ex

---
+++ Labs215Y2K/maxmin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class maxmin
+    {
+        public static void Ex1() 
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(0, 100);
+                Console.Write($" {array[i]}");
+            }
+            Console.WriteLine();
+            Console.Write("Для вывода минимального элемента нажмите ENTER");
+            Console.ReadLine();
+            int min = array[0];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (min > array[i])
+                    min = array[i];
+            }
+            Console.WriteLine("");
+            Console.WriteLine("минимальный элемент:" + min);
+            Console.ReadLine();
+        }
+    }
+}
