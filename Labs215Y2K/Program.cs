630a501fe567fd2f38bee5f5565a161520c708df
Alfrdedo <aimbot727@gmail.com>
2021-09-30 12:11:15 +0300

    Переворот числа

--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -6,7 +6,7 @@ namespace Labs215Y2K
     {
         static void Main(string[] args)
             {
-                square.Ex6();
+                perevorot.Ex7();
             }
         
     }
---
+++ Labs215Y2K/perevorot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class perevorot
+    {
+        public static void Ex7() 
+        {
+            int number = int.Parse(Console.ReadLine());
+            while (number > 0)
+            {
+                {
+                    Console.Write(number % 10);
+                    number = number / 10;
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
