3687a749df2a2119b15dd96e2dbe7507d41dca85
guala228 <dmitirylolov0@gmail.com>
2021-10-01 15:55:15 +0300

    home work all

---
+++ Labs215Y2K/calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class calculator
+    {
+        public static void ex4()
+        {
+            int a, b;
+
+            float result;
+            Console.WriteLine("enter 1st number= ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter 2nd number= ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите действие");
+            string opperation = Console.ReadLine();
+            switch (opperation)
+            {
+                case "+":
+                    result = a + b;
+                    Console.WriteLine(result);
+                    break;
+                case "-":
+                    result = a - b;
+                    Console.WriteLine(result);
+                    break;
+                case "*":
+                    result = a * b;
+                    Console.WriteLine(result);
+                    break;
+                case "/":
+                    result = a / b;
+                    Console.WriteLine(result);
+                    break;
+                case "%":
+                    result = a % b;
+                    Console.WriteLine(result);
+                    break;
+                default:
+                    Console.WriteLine("Я не знаю такой операции");
+                    break; ;
+            }
+        }
+    }
+}
---
+++ Labs215Y2K/minmax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class minmax
+    {
+        public static void ex4()
+        {
+            int[] myArray = new int[5] { 10, 3, 4, 15, 123 };
+            int minValue = myArray[0];
+            int maxValue = myArray[0];
+           
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+
+                Console.WriteLine(myArray[i]);
+                if (myArray[i] < minValue)
+                {
+                    minValue = myArray[i];
+                }
+                if (myArray[i] > maxValue)
+                {
+                    maxValue = myArray[i];
+                }
+
+            }
+            Console.WriteLine($"Минимальное значение массива {minValue}");
+            Console.WriteLine($"Максимальное значение массива {maxValue}");
+        }
+    }
+}
