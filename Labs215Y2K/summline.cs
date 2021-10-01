3a2a6b6272bfc3d67d0761f747c3f61bc350da8e
Rufat1k <rufatusmanov6@gmail.com>
2021-10-01 16:34:58 +0300

    summline.cs

---
+++ Labs215Y2K/summline.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class summline
+    {
+        public static void Summline()
+        //сумма строки
+        {
+            Random z = new Random();
+            int[,] array = new int[3, 3];
+
+            for (int y = 0; y < array.GetLength(0); y++)
+            {
+                for (int x = 0; x < array.GetLength(1); x++)
+                {
+                    array[y, x] = z.Next(10, 100);
+                    Console.Write($"{ array[y, x]} ");
+
+                }
+                Console.WriteLine();
+            }
+
+            int sum = 0;
+
+            for (int y = 0; y < array.GetLength(0); y++)
+            {
+                for (int x = 0; x < array.GetLength(1); x++)
+                {
+                    sum += array[y, x];
+                }
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+            Console.ReadKey();
+            Console.WriteLine(sum);
+
+        }
+    }
+}
