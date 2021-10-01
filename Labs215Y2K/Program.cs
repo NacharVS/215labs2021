f9d7fbc0c0bbd7763df1f1851ad831cce1892307
r3dinc <r3dinc@yandex.ru>
2021-10-01 15:27:30 +0300

    extra work

--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -7,12 +7,21 @@ namespace Labs215Y2K
     {
         static void Main(string[] args)
         {
+            //Заполнить двумерный массив по следующему правилу, массив произвольной размерности, массив передается через параметр:
+            //1 2 3 4 5
+            //2 1 2 3 4
+            //3 2 1 2 3
+            //4 3 2 1 2
+            //5 4 3 2 1
+
             int[,] arrayGlobal = new int[5, 5];
             ArrayOperations.TwoDimensionalArrayGeneration(arrayGlobal);
             int[,] arrayGlobal2 = new int[5, 5];
             Console.WriteLine();
             ArrayOperations.TwoDimensionalArrayGeneration(arrayGlobal2);
             Console.WriteLine(arrayGlobal[1,1]);
+
+
 
         }       
     }
