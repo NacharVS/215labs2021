1774bab87c100bdbcc8d2d9ef518e97f7c7e6581
F1laris <kflars2004@mail.ru>
2021-10-01 16:16:54 +0300

    New commit

--- Labs215Y2K/IdenticalElements.cs
+++ Labs215Y2K/IdenticalElements.cs
@@ -9,32 +9,35 @@ namespace Labs215Y2K
         public static void ex3()
         {
             int[] array1 = new int[10];
+
             for (int i = 0; i < array1.Length; i++)
             {
-                array1[i] = new Random().Next(0, 100);
-                Console.Write($"{array1[i]}");
+                array1[i] = new Random().Next(10, 40);
+                Console.Write($" {array1[i]}");
             }
-            Console.WriteLine();
+            Console.WriteLine("");
             int[] array2 = new int[10];
+
             for (int i = 0; i < array2.Length; i++)
             {
-                array2[i] = new Random().Next(0, 100);
-                Console.Write($"{array2}");
+                array2[i] = new Random().Next(10, 60);
+                Console.Write($" {array2[i]}");
             }
-            Console.WriteLine();
+
+            Console.WriteLine("");
+
             for (int i = 0; i < array1.Length; i++)
             {
-                for (int j = 0; j < array1.Length; j++)
+                for (int j = 0; j < array2.Length; j++)
                 {
-                    if (array1[i] == array2[i])
+                    if (array1[i] == array2[j])
                     {
-                        Console.Write($"{array1[i]}");
+                        Console.WriteLine(array1[i]);
+                        break;
                     }
                 }
             }
-            Console.WriteLine();
-
-        }
+        }               
     } 
     
 }
