de2957a45ab496101c79a19547a86b2be706df73
Rufat1k <rufatusmanov6@gmail.com>
2021-10-01 16:41:29 +0300

    Maxmin

--- Labs215Y2K/summline.cs
+++ Labs215Y2K/summline.cs
@@ -38,5 +38,32 @@ namespace Labs215Y2K
             Console.WriteLine(sum);
 
         }
+        public static void MaxMin()
+        {
+            int[] array = new int[10];
+            Random z = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = z.Next(100);
+                Console.WriteLine(array[i]);
+            }
+
+            int max = array[0];
+            int min = array[0];
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                    max = array[i];
+                if (array[i] < min)
+                    min = array[i];
+            }
+
+
+            Console.WriteLine("min" + min);
+            Console.WriteLine("max" + max);
+            Console.ReadKey();
+        }
     }
 }
