955536583d6b1e45b64d6177c8774ddf4092e807
r3dinc <r3dinc@yandex.ru>
2021-10-01 15:57:28 +0300

    ignore fix

---
+++ Labs215Y2K/massiviUprajneniya.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class massiviUprajneniya
+    {
+        public static void MaxMin()
+        {
+            int[] array = new int[10];
+
+        }
+    }
+}
