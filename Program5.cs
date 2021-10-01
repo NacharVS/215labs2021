d6c4bb98d09102ab9c1c541b178a769c699d4b79
kamilyamm <90504087+kamilyamm@users.noreply.github.com>
2021-10-01 15:50:41 +0300

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace perebor
+    {
+        class Program
+        {
+            static void Perebor(string[] args)
+            {
+                int a = int.Parse(Console.ReadLine());
+                for (int x1 = 0; x1 <= 9; x1++)
+                {
+                    for (int x2 = x1 + 1; x2 <= 9; x2++)
+                    {
+                        for (int x3 = x2 + 1; x3 <= 9; x3++)
+                        {
+                            if ((x1 + x2 + x3 == a) && (x1 != x2) && (x1 != x3) && (x2 != x3))
+                            {
+                                Console.Write(x1);
+                                Console.Write(x2);
+                                Console.WriteLine(x3);
+                            }
+                            else Console.Write("");
+                        }
+
+                    }
+                }
+            }
+        }
+    }
