24074b49539c25e604f3d766f34eaac86b396212
guala228 <dmitirylolov0@gmail.com>
2021-10-01 16:23:12 +0300

    ex

---
+++ Labs215Y2K/botkrutoi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class botkrutoi
+    {
+        public static void ex10()
+        {
+            Console.WriteLine("Привет! как тебя зовут!");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Рад знакмоству, {name}");
+            Console.WriteLine($"{name}, Сколько тебе лет?");
+            int num = int.Parse(Console.ReadLine());
+            if (num > 18.23)
+                Console.WriteLine("Тебе больше 18");
+            else if (num < 10)
+                Console.WriteLine("Ты ещё очень маленький");
+            else
+                Console.WriteLine("Ты ещё маленький");
+
+        }
+    }
+}
---
+++ Labs215Y2K/sum of string 2darrays1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class sum_of_string_2darrays1
+    {
+        public static void ex3()
+        {
+            int[,] array = new int[5, 4];
+            int sum = 0;
+
+
+
+            Random z = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = z.Next(100);
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+
+            }
+
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+
+
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+
+
+                    sum += array[i, j];
+
+                }
+
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+        }
+
+    }
+}
