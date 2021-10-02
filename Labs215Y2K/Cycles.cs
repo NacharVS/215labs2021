d630e8fc6a765d1795a15f3031ef24e476592f1c
milka228 <footbalthug1337@mail.ru>
2021-10-02 12:54:27 +0300

    exercises

--- Labs215Y2K/Cycles.cs
+++ Labs215Y2K/Cycles.cs
@@ -6,17 +6,41 @@ namespace Labs215Y2K
 {
     class Cycles
     {
-        static void StepenChisla()
+        public static void StepenChisla()
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine();
-            Console.WriteLine();
+            Console.WriteLine("Введите число, которое хотите возвести в степень");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите степень числа");
+            int b = int.Parse(Console.ReadLine());
+            int c = 1;
+            for (int i = 1; i <= b; i++)
+            {
+                c = m * c;
+            }
+            Console.WriteLine($"полученное число - {c}");
         }
 
-        static void OdinakoviyCifer()
+        public static void doorcode()
         {
-            Console.WriteLine(";oreighposdihgd");
-        }
+            int a = int.Parse(Console.ReadLine());
+            for (int x1 = 0; x1 <= 9; x1++)
+            {
+                for (int x2 = x1 + 1; x2 <= 9; x2++)
+                {
+                    for (int x3 = x2 + 1; x3 <= 9; x3++)
+                    {
+                        if ((x1 + x2 + x3 == a) && (x1 != x2) && (x1 != x3) && (x2 != x3))
+                        {
+                            Console.Write(x1);
+                            Console.Write(x2);
+                            Console.WriteLine(x3);
+                        }
+                        else Console.Write("");
+                    }
 
+                }
+
+            }
+        }
     }
 }
---
+++ Labs215Y2K/arrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class arrays
+    {
+        public static void first()
+        {
+            int[] array = new int[10];
+            int sum = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(10, 100);
+                {
+                    int[] numbers = new int[] { i };
+
+                    for (int i1 = 0; i1 < numbers.Length; i1++)
+                    {
+                        int value = numbers[i1];
+                        sum += value;
+                    }
+                }
+                Console.Write($" {array[i]}");
+                Console.WriteLine("Сумма элементов массива: " + sum);
+            }
+        }
+    }
+}
