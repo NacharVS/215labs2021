0980c74cc5b2bf1e8e8d41551433db49b090a332
Zarina666 <Sharafieva-zarina@mail.ru>
2021-10-02 12:03:06 +0300

    ex1 add

--- Labs215Y2K/massiviUprajneniya.cs
+++ Labs215Y2K/massiviUprajneniya.cs
@@ -6,7 +6,34 @@ namespace Labs215Y2K
 {
     class massiviUprajneniya
     {
-        public static void MaxMin()
+        public static void ex1()
+        //сумма элементов каждоый строки массива
+        {
+            int[,] array = new int[10, 10];
+            int sum = 0;
+            int a = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(1, 10);
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.WriteLine("   ");
+            }
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    sum = sum + a + array[i, j];
+
+                }
+                Console.WriteLine("");
+                Console.Write($"Сумма строки = {sum}");
+                sum = 0;
+            }
+        }
+            public static void MaxMin()
         {
             int[] array = new int[10];
 
