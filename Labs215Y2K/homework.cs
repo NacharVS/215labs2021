d49069a54cbbd0ee4c1da80b0cf9803bddb4ec67
YurovYan <yan.yurov.2005@mail.ru>
2021-10-02 12:20:51 +0300

    bot

---
+++ Labs215Y2K/homework.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class homework
+    {
+        public static void ex1()
+        {
+            Console.WriteLine("Привет, меня зовут Серёжа! А как тебя зовут?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name);
+            Console.WriteLine("Как дела?");
+            string dela = Console.ReadLine();
+            Console.WriteLine("У меня тоже.");
+            Console.WriteLine("Что делаешь?");
+            String delaesh = Console.ReadLine();
+            Console.WriteLine("Я сейчас занят, пока.");
+        }
+
+    }
+}
