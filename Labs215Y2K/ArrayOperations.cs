dee646f42f28cab56c8ae6abf4045627696536a1
Gazhos215 <9253924@mail.ru>
2021-10-04 16:49:00 +0300

    04.10.21

---
+++ Labs215Y2K/ArrayOperations.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class ArrayOperations
+    {
+        public static void OperationOne()
+        {
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(10, 100);
+
+                Console.Write($" {array[i]}");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] < array[i + 1])
+                {
+                    Console.Write($" {array[i + 1]}");
+                }
+            }
+        }
+
+        public static void ArrayGeneration (int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next();
+                Console.WriteLine($" {array[i]}");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+
+        public static void ArrayGeneration (int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(10, 100);
+                    Console.WriteLine($" {array[i, j]}");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+
+
+    }
+}
--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -6,13 +6,7 @@ namespace Labs215Y2K
     {
         public static void Main(string[] args)
         {
-            // SumMatrix.SumMatrix.ex1(); //
-            // Diagonal.Diagonal.ex2(); //
-            //Закон_Ома.Om.ex3();
-            //Площадь_поверхности_шара.ball.ex4();
-            //Поменять_значения_A_и_B.exchange.ex5();
-            //degree.num.ex6();
-            massiv.MinMax.ex8();
+            Labs215Y2K.ArrayOperations.OperationOne();
         }
     }
 }
