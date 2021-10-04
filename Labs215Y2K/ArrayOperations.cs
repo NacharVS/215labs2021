ab2af67a08f420383b2068561cffd1a1220625ff
F1aris <kflars2004@mail.ru>
2021-10-04 18:19:14 +0300

    New commit

--- Labs215Y2K/ArrayOperations.cs
+++ Labs215Y2K/ArrayOperations.cs
@@ -4,28 +4,40 @@ namespace Labs215Y2K
 {
     class ArrayOperations
     {
-        // 1. Найти максимальный и минимальный элемент массива
-        // 2. сумма и произведение 2х массивов по элементно
-        // 3. найти одинаковые элементы двух массивов
-        static void OperationOne()
+        public static void TwoDemitionalsArray(int[,] array)
         {
-            int[] array = new int[10];
-
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                array[i] = new Random().Next(10, 100);
-
-                Console.Write($" {array[i]}");
-            }
-
-            Console.WriteLine();
-
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                if (array[i] < array[i + 1])
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    Console.Write($" {array[i + 1]}");
+                    array[i, j] = new Random().Next();
+                    Console.Write($" {array[i, j]}");
                 }
+                Console.WriteLine(" ");
+            }
+        }
+        public static void TwoDemitionalsArray(int[,] array,  int a= int.MaxValue)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(int.MaxValue);
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+        public static void TwoDemitionalsArray(int[,] array, int a = int.MaxValue, int b = int.MinValue)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(a, b);
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.WriteLine(" ");
             }
         }
     }
