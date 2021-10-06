9d0acdb3564bfb13f5dc87cbdf765cba2b5db30e
r3dinc <r3dinc@yandex.ru>
2021-10-06 10:54:56 +0300

    06.10.2021

--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -5,24 +5,10 @@ namespace Labs215Y2K
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            //Заполнить двумерный массив по следующему правилу, массив произвольной размерности, массив передается через параметр:
-            //1 2 3 4 5
-            //2 1 2 3 4
-            //3 2 1 2 3
-            //4 3 2 1 2
-            //5 4 3 2 1
-
-            int[,] arrayGlobal = new int[5, 5];
-            ArrayOperations.TwoDimensionalArrayGeneration(arrayGlobal);
-            int[,] arrayGlobal2 = new int[5, 5];
-            Console.WriteLine();
-            ArrayOperations.TwoDimensionalArrayGeneration(arrayGlobal2);
-            Console.WriteLine(arrayGlobal[1,1]);
-
-
-
+            
         }       
     }
 }
---
+++ Labs215Y2K/RefOutParamsAndRecoursion.cs
@@ -0,0 +1,28 @@
+﻿namespace Labs215Y2K
+{
+    class RefOutParamsAndRecoursion
+    {
+        static int RecursFactorial(int x)
+        {
+            if (x > 0)
+            {
+                return x * RecursFactorial(x - 1);
+            }
+            else
+                return 1;
+        }
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Sum(int a, int b, out int res)
+        {
+
+            res = a + b;
+        }
+
+
+
+    }
+}
