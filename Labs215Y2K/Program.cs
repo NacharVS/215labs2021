6048d4794a14f48ae0472b0781aef770ae181190
SBG2K20 <sbg2k19@gmail.com>
2021-10-06 23:21:34 +0300

    I just do this exercise on monday

--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -4,9 +4,42 @@ namespace Labs215Y2K
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void TwoDemitionalsArray(int[,] array)
         {
-            //Я наконец-то разобрался с этой штукой
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next();
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.WriteLine(" ");
+            }
         }
+        public static void TwoDemitionalsArray(int[,] array, int b = int.MinValue)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(b);
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+        public static void TwoDemitionalsArray(int[,] array, int b = int.MaxValue, int c = int.MinValue)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(b, c);
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+
     }
 }
