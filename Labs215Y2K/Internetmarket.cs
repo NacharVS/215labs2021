91f528a56ef55c58621cd966f494867935066e0a
marcelomrv <kuzyakovmarsel@gmail.com>
2021-10-07 14:15:38 +0300

    start

---
+++ Labs215Y2K/Internetmarket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class Internetmarket
+    {
+        public static void catalog()
+        {
+            string[] array = new string[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                string a = "Буханка хлеба 30р";
+                array[i] = a;
+                Console.Write($" {array[i]}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -6,10 +6,9 @@ namespace Labs215Y2K
     {
         static void Main(string[] args)
         {
-            int[,] a = new int[10, 10];
-            int[] b = new int[10];
-            //ArrayOperations.TwoDemitionalsArray(a,100,1000);
-            ArrayOperations.TwoDemitionalsArray(a, 10,100);
+            int[] array = new int[10];
+            Internetmarket.catalog();
+            
         }
     }
 }
