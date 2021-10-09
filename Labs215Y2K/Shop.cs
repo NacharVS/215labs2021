d7864aefbefa6dc4348aa6f35e365d59d2a53416
F1laris <kflars2004@mail.ru>
2021-10-09 11:40:51 +0300

    COMMIT

---
+++ Labs215Y2K/Shop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class Shop
+    {
+        public static void Catalog()
+        {
+            string [] arrayList = {1}
+        }
+    }
+}
