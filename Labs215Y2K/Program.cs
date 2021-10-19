9dadfc66dd8d1d6fb66567a1596a81e127070806
Gazhos215 <9253924@mail.ru>
2021-10-19 23:58:01 +0300

    units +- 90%

--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -11,7 +11,8 @@ namespace Labs215Y2K
             ////ArrayOperations.ArrayGeneration(qq, 11, 50);
             //ArrayOperations.ArrayGeneration(qqnya, 2);
             //Magazin.Ashan.catalog();
-            StudentsSort.StudentsSort.StudentSort();
+            //StudentsSort.StudentsSort.StudentSort();
+            Program.UnitsCreator();
         }
         public static void magaz(int[] numberofproduct, int yesno, string[] catalog, string[] korzina, int counter, int cycleoperation, int[] countofproduct, int[] korzinakolvo, int[] price, int[] Sum, int[] sumtwo, int[] korzinaprice)
         {
@@ -78,26 +79,24 @@ namespace Labs215Y2K
 
         public static void UnitsCreator()
         {
-            Units.Builder Builder = new Units.Builder("Builder", 50, 10, 5, 30, 10);
-            Console.WriteLine($"{Builder.name} {Builder.health} {Builder.damage} { Builder.armor} { Builder.speed} { Builder.atackspeed}");
 
-            Units.Lumberjack Lumberjack = new Units.Lumberjack("Builder", 50, 10, 5, 30, 10);
-            Console.WriteLine($"{Lumberjack.name} {Lumberjack.health} {Lumberjack.damage} { Lumberjack.armor} { Lumberjack.speed} { Lumberjack.atackspeed}");
+            Units.Builder Builder = new Units.Builder("Builder", 50, 10, 15, 30, 10);
+            Console.WriteLine($"Имя - {Builder.name} Здоровье - {Builder.health} Урон - {Builder.damage} Броня - {Builder.armor} Скорость - {Builder.speed} Скорость атаки - {Builder.atackspeed}");
 
-            Units.Miners Miner = new Units.Miners("Builder", 50, 10, 5, 30, 10);
-            Console.WriteLine($"{Builder.name} {.health} {.damage} { .armor} { .speed} { .atackspeed}");
+            Units.Lumberjack Lumberjack = new Units.Lumberjack("Lumberjack", 60, 10, 15, 40, 15);
+            Console.WriteLine($"Имя - {Lumberjack.name} Здоровье - {Lumberjack.health} Урон - {Lumberjack.damage} Броня - {Lumberjack.armor} Скорость - { Lumberjack.speed} Скорость атаки - {Lumberjack.atackspeed}");
 
-            Units.Wariors Warior = new Units.Wariors("Builder", 50, 10, 5, 30, 10);
-            Console.WriteLine($"{Builder.name} {.health} {.damage} { .armor} { .speed} { .atackspeed}");
+            Units.Miners Miner = new Units.Miners("Miner", 65, 10, 15, 45, 20);
+            Console.WriteLine($"Имя - {Miner.name} Здоровье - {Miner.health} Урон - {Miner.damage} Броня - {Miner.armor} Скорость - {Miner.speed} Скорость атаки - {Miner.atackspeed}");
 
-            Units.Mages Mage = new Units.Mages("Builder", 50, 10, 5, 30, 10);
-            Console.WriteLine($"{Builder.name} {.health} {.damage} { .armor} { .speed} { .atackspeed}");
+            Units.Wariors Warior = new Units.Wariors("Warior", 100, 50, 50, 20, 45);
+            Console.WriteLine($"Имя - {Warior.name} Здоровье - {Warior.health} Урон - {Warior.damage} Броня - {Warior.armor} Скорость - {Warior.speed} Скорость атаки - {Warior.atackspeed}");
 
-            Units.Archers Archer = new Units.Archers("Builder", 50, 10, 5, 30, 10);
-            Console.WriteLine($"{Builder.name} {.health} {.damage} { .armor} { .speed} { .atackspeed}");
+            Units.Mages Mage = new Units.Mages("Mage", 120, 100, 45, 30, 30);
+            Console.WriteLine($"Имя - {Mage.name} Здоровье - {Mage.health} Урон -{Mage.damage} Броня -{Mage.armor} Скорость - {Mage.speed} Скорость атаки - {Mage.atackspeed}");
+
+            Units.Archers Archer = new Units.Archers("Archer", 80, 90, 30, 40, 40);
+            Console.WriteLine($"Имя - {Archer.name} Здоровье - {Archer.health} Урон - {Archer.damage} Броня - {Archer.armor} Скорость - {Archer.speed} Скорость атаки - {Archer.atackspeed}");
         }
-    }
-}
-
     }
 }
