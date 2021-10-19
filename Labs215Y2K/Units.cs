5ad4c9f16a25ad9b543fced1bad679a85937d4c9
Gazhos215 <9352921@mail.ru>
2021-10-19 16:17:39 +0300

    gg

--- Labs215Y2K/Units.cs
+++ Labs215Y2K/Units.cs
@@ -1,10 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Labs215Y2K
+namespace Units
 {
-    class Units
+    class Unit
     {
+        public string name;
+        public int health;
+        public int damage;
+        public int armor;
+        public int speed;
+        public int atackspeed;
+
+      
+
+        public void Work()
+        {
+            Console.WriteLine($"{name} is working");
+        }
+    }
+    class Work : Unit
+    {
+        public Unit(string name, int health, int damage, int armor, int speed, int atackspeed)
+        {
+            this.name = name;
+            this.health = health;
+            this.damage = damage;
+            this.armor = armor;
+            this.speed = speed;
+            this.atackspeed = atackspeed;
+        }
     }
 }
