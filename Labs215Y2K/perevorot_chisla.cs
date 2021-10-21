123b003e7f0d9afc5aed0126e33ade1f8db0723b
bulatboba <safikovbulan6@gmail.com>
2021-10-21 22:12:01 +0300

    perevorot

---
+++ Labs215Y2K/perevorot_chisla.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class perevorot_chisla
+    {
+        public static void r1()
+        {
+            int a = 0;
+            int ara = int.Parse(Console.ReadLine());
+            while (ara > 0)
+            {
+                a *= 10;
+
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
