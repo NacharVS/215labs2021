9ef7438261d25588dbaa7c671db810374bcb0095
alnkmrt <aln.kmrtnv@gmail.com>
2021-10-21 15:10:35 +0300

    dd

--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -6,9 +6,14 @@ namespace Labs215Y2K
     {
         static void Main(string[] args)
         {
-            OfficeWorker person1 = new OfficeWorker("Клементий", "работник офиса", 40, 20, 80);
-            Console.WriteLine($" Имя персонажа - {person1.name}, специальность - {person1.work}, здоровье - {person1.health}, сила - {person1.damage}, стамина - {person1.stamina}");
-            person1.Move();
+            Warrior warrior = new Warrior("Warrior", 1000, 3600, 40, 450, 30, 45);
+            Archer archer = new Archer("Archer", 1500, 3000, 50, 500, 40, 75, 300, 12);
+            Healer healer = new Healer("Healer", 3000, 5000, 20, 150, 20, 30);
+            Wizard wizard = new Wizard("Wizard", 2000, 3700, 100, 700, 60, 50);
+            Guardian guardian = new Guardian("Guardian", 4000, 6000, 50, 600, 20, 30);
+            warrior.Move();
+            wizard.Attack();
+            archer.ArcherAttack(); 
         }
     }
 }
--- Labs215Y2K/Unit.cs
+++ Labs215Y2K/Unit.cs
@@ -6,19 +6,24 @@ namespace Labs215Y2K
 {
     class Unit
     {
-        public string name;
-        public string work;
-        public int health;
-        public int damage;
-        public int stamina;
+        public string typeUnit;
+        public int currenthealth;
+        public int maxhealth;
+        public int mindamage;
+        public int maxdamage;
+        public int speed;
+        public int attackspeed;
+
 
         public void Move()
         {
-            Console.WriteLine($"{name} is moving");
+            Console.WriteLine($"{typeUnit} is moving");
         }
-        public void Work()
+        public void Attack()
         {
-            Console.WriteLine($"{name} is working");
+            int hit = new Random().Next(mindamage, maxdamage);
+            Console.WriteLine($"{typeUnit} has dealt {hit} units of damage ");
         }
+
     }
 }
