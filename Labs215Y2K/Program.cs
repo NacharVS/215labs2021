1113516a8b950777e3dcf6f3eef462ea480d12e3
bulatboba <safikovbulan6@gmail.com>
2021-10-21 22:47:38 +0300

    arraymassivi

--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -6,9 +6,7 @@ namespace Labs215Y2K
     {
         static void Main(string[] args)
         {
-            int[,] qq = new int[10,10];
-            int[] qq1 = new int[10];
-            ArrayOperations.ArrayGeneration(qq, 0, 2);
+            perevorot_chisla.r3();
         }
     }
 }
--- Labs215Y2K/perevorot_chisla.cs
+++ Labs215Y2K/perevorot_chisla.cs
@@ -8,14 +8,61 @@ namespace Labs215Y2K
     {
         public static void r1()
         {
-            int a = 0;
-            int ara = int.Parse(Console.ReadLine());
-            while (ara > 0)
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
             {
-                a *= 10;
-
+                array[i] = new Random().Next(-50, 50);
+                
+                if (array[i]<0)
+                {
+                    Console.Write($" {array[i]}");
+                }
             }
-            Console.WriteLine(a);
+        }
+        public static void r2()
+        {
+            int[] array = new int[10];
+            int maxnumber = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(0, 100);
+                Console.Write($" {array[i]}");
+                {
+                    if (maxnumber < array[i])
+                    {
+                        maxnumber = array[i];
+                        
+                    }
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"Максимальное число массива: {maxnumber}");
+        }
+        public static void r3()
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(0, 10);
+                Console.Write($" {array[i]} ");
+            }
+            Console.WriteLine("");
+            int[] array1 = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array1[i] = new Random().Next(0, 10);
+                Console.Write($" {array1[i]} ");
+            }
+            Console.WriteLine();
+            Console.Write("-----------------------------------");
+            Console.WriteLine();
+            int[] array2 = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array2[i] = array1[i] + array[i];
+                Console.Write($" {array2[i]} ");
+            }
+
         }
     }
 }
