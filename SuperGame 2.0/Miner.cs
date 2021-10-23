36bdf96029ac9b63a43b852f88f0498c475309e8
foxenberg <daniel.korshunov.2001@gmail.com>
2021-10-23 12:50:00 +0300

    Super original and interesting game

---
+++ SuperGame 2.0/Miner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperGame_2._0
+{
+    class Miner:Unit
+    {
+        public Miner(string typeunitc, int workspeedc, int maxhealthc, int healthc, int speedc, int attackspeedc, int mindamagec, int maxdamagec, string skillc)
+        {
+            typeunit = typeunitc;
+            workspeed = workspeedc;
+            maxhealth = maxhealthc;
+            health = healthc;
+            speed = speedc;
+            attackspeed = attackspeedc;
+            mindamage = mindamagec;
+            maxdamage = maxdamagec;
+            skill = skillc;
+        }
+    }
+}
