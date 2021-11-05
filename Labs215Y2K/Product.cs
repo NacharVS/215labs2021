536b24d0234e72b2735d362029de8b787b659428
r3dinc <r3dinc@yandex.ru>
2021-11-05 13:22:41 +0300

    products

---
+++ Labs215Y2K/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class Product
+    {
+
+        // скидка 20% но не больше 500 у.е. 
+        private double _price;
+        private double _quantity;
+        private string _nameOfProduct;
+        private double _nds;
+
+        public Product(double price, double quantity, string nameOfProduct)
+        {
+            _price = price;
+            _quantity = quantity;
+            _nameOfProduct = nameOfProduct;
+            _nds = _price * 0.19;
+        }
+
+        public double Nds 
+        {
+            get 
+            {
+                return _nds;
+            }
+
+            set
+            {
+                if (value > _price * 0.19)
+                    _nds = _price * 0.19;
+                else
+                    _nds = value;
+            }
+        }
+    }
+}
--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -8,32 +8,12 @@ namespace Labs215Y2K
         
         static void Main(string[] args)
         {
-            Student[] students = new Student[3];
-            Student student1 = new Student();
-            student1.name = "Valeriy";
-            student1.group = 222;
-            student1.age = 16;
-            Student student2 = new Student();
-            student2.name = "Ivan";
-            student2.group = 223;
-            student2.age = 17;
-            Student student3 = new Student();
-            student3.name = "Sergey";
-            student3.group = 221;
-            student3.age = 18;
-
-            students[0] = student1;
-            students[1] = student2;
-            students[2] = student3;
-
-            foreach (var item in students)
-            {
-                if(item.age >= 18)
-                    Console.WriteLine(item.name);
-            }
-            
+            Product prod1 = new Product(55.59, 100, "Banana");
+            prod1.Nds = 10;
+            Console.WriteLine(prod1.Nds);
 
 
-        }       
+
+        }
     }
 }
