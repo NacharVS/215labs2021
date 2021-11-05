89382d12933933d05584998f64ab81e1b0ce2520
mavrova13 <nastamavrova@gmail.com>
2021-11-05 13:55:26 +0300

    ...

---
+++ MarketButNewVersion/MarketButNewVersion/InfoClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarketButNewVersion
+{
+    class InfoClass
+    {
+        private double _price;
+        private double _quantity;
+        private string _nameOfProduct;
+        private double _nds;
+
+        public InfoClass(double price, double quantity, string nameOfProduct)
+        {
+            _price = price;
+            _quantity = quantity;
+            _nameOfProduct = nameOfProduct;
+            _nds = _price * 0.19;
+        }
+
+        public double Nds
+        {
+            get
+            {
+                return _nds;
+            }
+
+            set
+            {
+                if (value > _price * 0.19)
+                    _nds = _price * 0.19;
+                else
+                    _nds = value;
+            }
+        }
+    }
+}
---
+++ MarketButNewVersion/MarketButNewVersion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarketButNewVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
