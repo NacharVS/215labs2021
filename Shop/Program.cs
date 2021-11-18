20eb7cc3f5c6c881d0fa27e29c583d974ea5824e
marcelomrv <kuzyakovmarsel@gmail.com>
2021-11-18 21:20:58 +0300

    1

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -6,7 +6,18 @@ namespace Shop
     {
         static void Main(string[] args)
         {
-            ShopNew1.start();
+            //ShopNew1.start();
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Введите размер массива");
+            int[] array = new int[b];
+            int sum;
+            for (int i = 1; i < array.Length;i+2)
+            {
+                if (array[i] / 2)
+                {
+
+                }
+            }
         }
     }
 }
