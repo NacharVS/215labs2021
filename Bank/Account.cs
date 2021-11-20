b1d18e5e9280c580fd3316ed6eeee2e9ce9ead3a
NikitaYstinin <152019@mck-ktits.ru>
2021-11-20 12:53:05 +0300

    updates

--- Bank/Account.cs
+++ Bank/Account.cs
@@ -34,25 +34,45 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Введите количество денег, которое Вы хотите внести");
             double cash = double.Parse(Console.ReadLine());
-            money += cash;
-            Console.WriteLine($"Поздравляем. {cash} рублей было зачислено на Ваш счёт.");
+            if (money > 0)
+            {
+                money += cash;
+                Console.WriteLine($"Поздравляем. {cash} рублей было зачислено на Ваш счёт.");
+            }
+            else
+                Console.WriteLine("Жулик, не воруй!");
         }
 
         public void Withdraw(ref double money)
         {
             Console.WriteLine("Введите количество денег, которое Вы хотите снять");
             double cash = double.Parse(Console.ReadLine());
-            money += cash;
-            Console.WriteLine($"{cash} рублей было снято со счёта.");
+            if (money > 0)
+            {
+                money += cash;
+                Console.WriteLine($"{cash} рублей было снято со счёта.");
+            }
+            else
+                Console.WriteLine("Жулик, не воруй!");
         }
 
         public static void Transaction(Account accSeller, Account accGetter)
         {
             Console.WriteLine($"Введите сумму, которую хотите перевести {accGetter.name}");
             double money = double.Parse(Console.ReadLine());
-            accSeller.balance -= money;
-            accGetter.balance += money;
-            Console.WriteLine($"Со счёта {accSeller.name} было переведено {accGetter.name} {money} рублей");
+            if (money > 0)
+            {
+                if (money <= accSeller.balance)
+                {
+                    accSeller.balance -= money;
+                    accGetter.balance += money;
+                    Console.WriteLine($"Со счёта {accSeller.name} было переведено {accGetter.name} {money} рублей");
+                }
+                else
+                    Console.WriteLine("На вашем счету недостаточно средств.");
+            }
+            else
+                Console.WriteLine("Жулик, не воруй!");
         }
     }
 }
