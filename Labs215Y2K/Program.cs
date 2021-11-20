73d1fae5b0b0072aa6475c1214f90a89d1b95dd3
F1laris <kflars2004@mail.ru>
2021-11-20 12:49:25 +0300

    Commit

--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -6,7 +6,20 @@ namespace Labs215Y2K
     {
         static void Main(string[] args)
         {
-            Shop.Catalog();
+            Account acc1 = new Account("Ivan", 5000);
+            Account acc2 = new Account("Fedor", 6000);
+            Account acc3 = new Account("Elena", 3000);
+            acc1.ShowInfo();
+            acc2.ShowInfo();
+            acc3.ShowInfo();
+            Console.WriteLine();
+            Account.rate = 0.09;
+            Account.ShowProfit(acc1, 1);
+            Account.ShowProfit(acc3, 1);
+            Account.ShowProfit(acc3, 1);
+            acc1.ShowInfo();
+            acc2.ShowInfo();
+            acc3.ShowInfo();
         }
     }
 }
---
+++ Labs215Y2K/bank.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class bank
+    {
+        public string name;
+        public double balance;
+
+        public static double rate = 0.09;
+
+        public void Account(string name, double balance)
+        {
+            this.name = name;
+            this.balance = balance;
+        }
+
+        public void ShowInfo()
+        {
+            System.Console.WriteLine($"{name} - {balance}");
+        }
+
+        public static void ShowProfit( Account acc, int month)
+        {
+            for (int i = 0; i < month; i++)
+            {
+                acc.balance += acc.balance * rate;
+            }
+        }
+
+        public void Deposit(double money)
+        {
+
+        }
+
+        public void Withdraw(double money)
+        {
+
+        }
+
+        public static void Transaction(Account accSeller, Account accGetter)
+        {
+
+        }
+    }
+}
