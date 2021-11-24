a6a001d919bd3d9afee8ec596d2d723b04a1a23f
F1laris <kflars2004@mail.ru>
2021-11-24 09:58:45 +0300

    comm

--- Labs215Y2K/bank.cs
+++ Labs215Y2K/bank.cs
@@ -6,5 +6,47 @@ namespace Labs215Y2K
 {
     class bank
     {
+        public string name;
+        public double balance;
+
+        public static double rate = 0.09;
+
+        public bank(string v1, int v2)
+        {
+        }
+
+        public void Account(string name, double balance)
+        {
+            this.name = name;
+            this.balance = balance;
+        }
+
+        public void ShowInfo()
+        {
+            System.Console.WriteLine($"{name} - {balance}");
+        }
+
+        public static void ShowProfit(bank acc, int month)
+        {
+            for (int i = 0; i < month; i++)
+            {
+                acc.balance += acc.balance * rate;
+            }
+        }
+
+        public void Deposit(double money)
+        {
+
+        }
+
+        public void Withdraw(double money)
+        {
+
+        }
+
+        public static void Transaction(bank accSeller, bank accGetter)
+        {
+
+        }
     }
 }
--- Labs215Y2K/program.cs
+++ Labs215Y2K/program.cs
@@ -8,7 +8,20 @@ namespace Labs215Y2K
     {
         public static void Main()
         {
-            shop.ex1();
+            bank acc1 = new bank("Ivan", 5000);
+            bank acc2 = new bank("Fedor", 6000);
+            bank acc3 = new bank("Elena", 3000);
+            acc1.ShowInfo();
+            acc2.ShowInfo();
+            acc3.ShowInfo();
+            Console.WriteLine();
+            bank.rate = 0.09;
+            bank.ShowProfit(acc1, 1);
+            bank.ShowProfit(acc3, 1);
+            bank.ShowProfit(acc3, 1);
+            acc1.ShowInfo();
+            acc2.ShowInfo();
+            acc3.ShowInfo();
         }
 
 
