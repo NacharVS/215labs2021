cbd9036bba89fad5d4aeffac94fe364378bfc605
DanilTasevcev215 <tasevcevdanil18@gmail.com>
2021-12-01 11:11:20 +0300

    Day 10

--- Игра/Program.cs
+++ Игра/Program.cs
@@ -2,30 +2,28 @@
 using Игра;
 
 
-
 namespace Игра
 {
     class Program
     {
+        static void Start()
+        {
+           
+            Client client = new Client();
+            while (true)
+            {
+                client.Popolnenue(client.BallansDollars, client.BallansRubles);
+                Console.WriteLine(client.BallansDollars);
+                Console.WriteLine(client.BallansRubles);
+
+            }
+        }
         static void Main()
         {
-            Peaceful p1 = new Peaceful(); 
-            Peaceful p2 = new Peaceful();
-            p1.Change_Name = "Валера";
-            p1.Change_New_hp = 100;
-            p2.Change_Name = "Данил";
-            p2.Change_New_hp = 100;
-            p1.Change_Damage = 20; 
-            p2.Change_Damage = 20;
-
-            for (int i = 0; i < 5; i++)
-            {
-                Action.Figth(p1, p2);
-                Console.WriteLine(p1.Change_New_hp);
-                Console.WriteLine(p2.Change_New_hp);
-            }
-            
+            Start();
         }
     }
-    
 }
+
+
+
