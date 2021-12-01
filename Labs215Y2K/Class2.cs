a716d1eb3ad8ff08d0c4047403483d00d8551a10
SBG2K20 <sbg2k19@gmail.com>
2021-12-01 10:40:05 +0300

    Только что загрузил

---
+++ Labs215Y2K/Class2.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class Account
+    {
+        private string _name;
+        private int _balance;
+        private int _age;
+        private string _id;
+        private int _yearOfBirth;
+
+        public string Name
+        {
+            get
+            {
+                return _name.ToUpper();
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public int Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                _balance = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+
+                return _age;
+            }
+            set
+            {
+                if (value > 1910 & value <= DateTime.Now.Year)
+                {
+                    _age = value = DateTime.Now.Year - value;
+                }
+                _age = value;
+
+            }
+        }
+        public string Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+        public int YearOfBirth
+        {
+            get
+            {
+                return _yearOfBirth;
+
+            }
+            set
+            {
+                _yearOfBirth = value;
+            }
+        }
+
+
+
+    }
+
+}
--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -4,42 +4,210 @@ namespace Labs215Y2K
 {
     class Program
     {
-        public static void TwoDemitionalsArray(int[,] array)
+        static void Main(string[] args)
         {
-            for (int i = 0; i < array.GetLength(0); i++)
+
+
+
+            Console.WriteLine(" Данные первого счёта");
+
+            Account acc1 = new Account();
+            acc1.Name = "Егор";
+            Console.WriteLine(acc1.Name);
+
+            acc1.Balance = 1000;
+            Console.WriteLine(acc1.Balance + " рублей");
+
+            acc1.Age = 16;
+            Console.WriteLine(acc1.Age);
+
+            acc1.Id = "AdDwdw23";
+            Console.WriteLine(acc1.Id);
+
+            acc1.YearOfBirth = 2004;
+            Console.WriteLine(acc1.YearOfBirth);
+
+
+            Console.WriteLine(" Данные второго счёта");
+
+
+            Account acc2 = new Account();
+
+            acc2.Name = "egor";
+            Console.WriteLine(acc2.Name);
+
+            acc2.Balance = 0;
+            Console.WriteLine(acc2.Balance + " долларов");
+
+            acc2.Age = 18;
+            Console.WriteLine(acc2.Age);
+
+            acc2.Id = "A1DD2D/UTG";
+            Console.WriteLine(acc2.Id);
+
+            acc2.YearOfBirth = 2006;
+
+            Console.WriteLine(acc2.YearOfBirth);
+
+            Console.WriteLine(" Данные другого счёта");
+
+            Account acc3 = new Account();
+            acc3.Name = "Артём";
+            Console.WriteLine(acc3.Name);
+
+            acc3.Balance = 1000;
+            Console.WriteLine(acc3.Balance + " рублей");
+
+            acc3.Age = 16;
+            Console.WriteLine(acc3.Age);
+
+            acc3.Id = "A2qsfd3";
+            Console.WriteLine(acc3.Id);
+
+            acc3.YearOfBirth = 2004;
+            Console.WriteLine(acc3.YearOfBirth);
+
+
+            Console.WriteLine(acc1.Balance + " " + acc2.Balance);
+
+
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("Перевод на второй счёт - нажмите 1");
+            Console.WriteLine(" Пополнение счёта - нажмите 2");
+            Console.WriteLine(" Перевод другому клиенту - нажмите 3");
+
+            int action = int.Parse(Console.ReadLine());
+
+            if (action == 1)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    array[i, j] = new Random().Next();
-                    Console.Write($" {array[i, j]}");
-                }
-                Console.WriteLine(" ");
+                Perevod(acc1, acc2);
             }
-        }
-        public static void TwoDemitionalsArray(int[,] array, int b = int.MinValue)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
+            if (action == 3)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    array[i, j] = new Random().Next(b);
-                    Console.Write($" {array[i, j]}");
-                }
-                Console.WriteLine(" ");
+                Perevod(acc1, acc3);
             }
-        }
-        public static void TwoDemitionalsArray(int[,] array, int b = int.MaxValue, int c = int.MinValue)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
+            else
             {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    array[i, j] = new Random().Next(b, c);
-                    Console.Write($" {array[i, j]}");
-                }
-                Console.WriteLine(" ");
+                Popolnenie(acc1, acc2);
             }
+
+            Console.WriteLine(acc2.Balance + " долларов");
+            Console.WriteLine(acc1.Balance + " рублей");
+
         }
 
+
+
+
+
+        public static void Perevod(Account acc1, Account acc2)
+        {
+            Console.WriteLine("Введите сумму");
+            int summaperevoda = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите действие");
+
+            Console.WriteLine("Конвертация рублей в доллары - нажмите 1");
+
+            Console.WriteLine("Конвертация долларов в рубли- нажмите 2");
+
+
+            int action = int.Parse(Console.ReadLine());
+            if (action == 1)
+            {
+                int cash = summaperevoda % 74;
+                acc1.Balance -= summaperevoda + cash;
+                summaperevoda = summaperevoda / 74;
+                acc2.Balance += summaperevoda;
+            }
+            else
+            {
+                summaperevoda = summaperevoda * 74;
+                acc1.Balance += summaperevoda;
+                acc2.Balance -= summaperevoda;
+            }
+
+            Console.WriteLine(acc2.Balance + " долларов");
+            Console.WriteLine(acc1.Balance + " рублей");
+
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("Перевод на второй счёт - нажмите 1");
+            Console.WriteLine(" Пополнение счёта - нажмите 2");
+
+
+            int action2 = int.Parse(Console.ReadLine());
+
+            if (action2 == 1)
+            {
+                Perevod(acc1, acc2);
+            }
+            else
+            {
+                Popolnenie(acc1, acc2);
+            }
+
+        }
+
+
+
+
+
+
+        public static void Popolnenie(Account acc1, Account acc2)
+        {
+
+            Console.WriteLine("Выберите действие");
+
+            Console.WriteLine("Пополнение первого счёта в рублях - нажмите 1");
+
+            Console.WriteLine(" Пополнение второго счёта в долларах - нажмите 2");
+
+            int action = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Введите сумму");
+
+            int summapopolneniya = int.Parse(Console.ReadLine());
+
+            if (action == 1)
+            {
+                acc1.Balance += summapopolneniya;
+            }
+            else
+            {
+                acc2.Balance += summapopolneniya;
+
+            }
+
+
+        }
+        public static void Perevoddrugu(Account acc1, Account acc3)
+        {
+            Console.WriteLine("Введите сумму для перевода другу");
+
+            int summaperevoda = int.Parse(Console.ReadLine());
+
+
+            acc1.Balance -= summaperevoda;
+
+            acc3.Balance += summaperevoda;
+
+
+            Console.WriteLine(acc3.Balance + " счёт друга");
+            Console.WriteLine(acc1.Balance + " рублей на вашем счёт");
+
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("Перевод на второй счёт - нажмите 1");
+            Console.WriteLine(" Пополнение счёта - нажмите 2");
+
+
+
+
+        }
+
+
     }
+
 }
+
+
