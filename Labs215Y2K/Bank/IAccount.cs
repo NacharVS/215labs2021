26d3b4cd08f3668a891141b5970106da5799d6d0
F1laris <kflars2004@mail.ru>
2021-12-04 10:08:54 +0300

    dd

--- Labs215Y2K/Bank/IAccount.cs
+++ Labs215Y2K/Bank/IAccount.cs
@@ -6,5 +6,12 @@ namespace Labs215Y2K
 {
     interface IAccount
     {
+
+        public void ShowInfo(Account acc);
+        public void ShowProfit(Account acc, int mouths);
+        public void Withdraw(Account acc);
+        public void Transaction(Account acc, Account accGetter);
+        public void Deposit(Account acc);
+
     }
 }
