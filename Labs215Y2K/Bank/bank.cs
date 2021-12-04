c6f2fb248d5e3d12d571d745cf71ac5c6d85ddea
F1laris <kflars2004@mail.ru>
2021-12-04 10:07:30 +0300

    gf

--- Labs215Y2K/Bank/bank.cs
+++ Labs215Y2K/Bank/bank.cs
@@ -4,7 +4,101 @@ using System.Text;
 
 namespace Labs215Y2K
 {
-    class bank
-    {
-    }
+    
+        class Account : IAccount
+        {
+            public delegate void NameValueChangeDelagate(string name, string Oldname);
+            public event NameValueChangeDelagate NameChangeEvent;
+
+
+            private string _name;
+            private double _balance;
+            private int _id;
+
+            public static double rate = 0.09;
+
+            public string Name
+            {
+                get => _name;
+                set
+                {
+                    string Oldname = _name;
+                    _name = value;
+                    NameChangeEvent?.Invoke(_name, Oldname);
+                }
+            }
+            public double Balance { get => _balance; set => _balance = value; }
+            public int Id { get => _id; set => _id = value; }
+
+            public Account(string name, double balance, int id)
+            {
+                this.Name = name;
+                this.Balance = balance;
+                this.Id = id;
+            }
+
+            public void ShowInfo(Account acc)
+            {
+                if (acc.Balance > 0)
+                {
+                    Console.WriteLine($"Пользователь - {Name} \t Баланс - {Balance}   \t _id - {Id}");
+                }
+            }
+
+            public void ShowProfit(Account acc, int month)
+            {
+                for (int i = 0; i < month; i++)
+                {
+                    acc.Balance += acc.Balance * rate;
+                }
+            }
+
+            public void Deposit(Account acc)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"{acc.Name} - {acc.Balance}");
+                Console.WriteLine("Укажите сумму, которую вы хотите внести");
+                double DepositMoney = double.Parse(Console.ReadLine());
+                acc.Balance = acc.Balance + DepositMoney;
+            }
+
+            public void Withdraw(Account acc)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"{acc.Name} - {acc.Balance}");
+                Console.WriteLine("Укажите сумму, которую вы хотите вывести");
+                double WithdrawMoney = double.Parse(Console.ReadLine());
+                acc.Balance = acc.Balance - WithdrawMoney;
+                if (acc.Balance < 0)
+                {
+                    Console.WriteLine("На балансе недостаточно средств");
+
+                }
+            }
+
+            public void Transaction(Account acc, Account accGetter)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Выберите Id пользователя от которого вы хотите перевести");
+                int AccGetterSeller = int.Parse(Console.ReadLine());
+
+                if (AccGetterSeller == acc.Id)
+                {
+                    Console.WriteLine($"Укажите сумму, которую вы хотите перевести");
+                    double TransactionMoney = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Выберите Id пользователя которому вы хотите перевести");
+                    int AccGetterId = int.Parse(Console.ReadLine());
+                    if (AccGetterId == accGetter.Id)
+                    {
+
+                        acc.Balance -= TransactionMoney;
+                        Console.WriteLine($"{acc.Name} - {acc.Balance}");
+                        accGetter.Balance += TransactionMoney;
+                    }
+                }
+            }
+
+
+        }
+    
 }
