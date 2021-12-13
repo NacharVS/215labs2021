0cd4142f6233b94ef41b97e7b5f8bc3b87912ab8
marcelomrv <kuzyakovmarsel@gmail.com>
2021-12-13 22:12:16 +0300

    1

--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -11,6 +11,12 @@ namespace Labs215Y2K
 
             //int[] priceproducts = new int[10] { 30, 8, 40, 250, 50, 70, 36, 13, 75, 150 };
             //int[] productschoose = new int[10];
+            int[] array = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(1, 5);
+                Console.Write($"{array[i]} ");
+            }
         }
     }
 }
