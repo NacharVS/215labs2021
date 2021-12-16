91c7cc18d703d0516ed3ddbf51af4bb194e29090
marcelomrv <kuzyakovmarsel@gmail.com>
2021-12-16 15:10:15 +0300

    ahah

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -6,8 +6,15 @@ namespace Shop
     {
         static void Main(string[] args)
         {
-            ShopNew1.start();
-            
+            //ShopNew1.start();
+            string a = "BBlat";
+            for (int i = a.Length -1;i >=0;i--)
+            {
+                Console.Write($"{a[i]}");
+            }
+            a = a.Remove(0, 2);
+            Console.WriteLine();
+            Console.WriteLine(a);
         }
     }
 }
