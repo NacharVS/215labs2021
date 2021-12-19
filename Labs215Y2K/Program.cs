7a58c8cedce430167c9c70b92cb55c31d743e4c9
sal1wan <salopechkin@mail.ru>
2021-12-19 21:41:35 +0300

    RPG

--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -1,17 +1,89 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
-namespace Labs215Y2K
+namespace New_Game
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Shop.Practice();
+            Work();
+            Console.ReadKey(); 
+            
         }
-        static void buy(string[] args) 
+
+       
+
+        public static void Work()
         {
-            Shop.BuyProducts();
+
+            Unit dualunit = new Unit(0, 0, 0, 0, "");
+            Warrior war = new Warrior(30, 15, 10, 85, "воин1");
+            Warrior war2 = new Warrior(50, 35, 30, 45, "воин2");
+            war.testevent += Unit.DeBuff;
+
+            List<Unit> Chaptres = new List<Unit>();
+
+            Chaptres.Add(war);
+            Chaptres.Add(war2);
+
+            //Расчёт характеристик
+
+            for (int i = 0; i < Chaptres.Count; i++)
+            {
+                Chaptres[i].CurrencyHealth = Chaptres[i].MaxHealth;
+                Chaptres[i].CurrencyMana = Chaptres[i].MaxMana;
+
+
+
+                //Chaptres[i].M_Attack = Chaptres[i].Intelligence * 4;
+                //Chaptres[i].M_CritChance = 20 + Chaptres[i].Intelligence * 0.1;
+                //Chaptres[i].M_CritDamage = Chaptres[i].M_Attack * (2 + Chaptres[i].Intelligence * 0.15);
+                Console.WriteLine($"Магический Урон - {Chaptres[i].M_Attack} - Шанс магического крита. Урона-  {Chaptres[i].M_CritChance} % Крит. урон магический - {Chaptres[i].M_CritDamage}");
+
+
+                
+                //Chaptres[i].CurrencyMana = Chaptres[i].MaxMana = Chaptres[i].Intelligence * 3;
+
+
+                //Chaptres[i].P_Attack = Chaptres[i].Strength * 3 + Chaptres[i].Dexterity * 0.5;
+                //Chaptres[i].P_Defense = Chaptres[i].Constitution * 0.5 + Chaptres[i].Dexterity * 3;
+                //Chaptres[i].M_Defense = Chaptres[i].Intelligence * 2;
+                //Chaptres[i].P_CritChance = 20 + Chaptres[i].Dexterity * 0.3;
+                //Chaptres[i].P_CritDamage = Chaptres[i].P_Attack * (2 + Chaptres[i].Dexterity * 0.05);
+
+
+
+                Console.WriteLine($"Характеристика вашего персонажа: ХП- {Chaptres[i].CurrencyHealth} Мана- {Chaptres[i].CurrencyMana} Физ.Дамаг- {Chaptres[i].P_Attack} Физ.Защита - {Chaptres[i].P_Defense} Шанс Крита {Chaptres[i].P_CritChance} % Крит Урон - {Chaptres[i].P_CritDamage}");
+                Console.WriteLine($"Маг.Защита - {Chaptres[i].M_Defense}");
+                
+
+                if (Chaptres[i] is Warrior)
+                {
+                    Console.WriteLine("Ваш персонаж - Воин. Берите меч и идите в бой");
+                }
+                if (Chaptres[i] is Mage)
+                {
+                    Console.WriteLine("Вы - МАГомед, сошедший с пути становления в Подземелье ЛЮБЛИНО, отправились в дальний путь");
+                }
+                if (Chaptres[i] is Archer)
+                {
+                    Console.WriteLine("Охотник, которому прострелили колено , открывает для себя дорогу авантюриста.");
+                }
+                Unit.PAttack(war, Chaptres);
+                Chaptres[i].CurrencyHealth = Chaptres[i].MaxHealth = 120;
+                Unit.PAttack(war, Chaptres);
+                Console.WriteLine(war.P_Defense); 
+
+
+
+            }
+        
+             
+          
+  
         }
+
+
     }
 }
