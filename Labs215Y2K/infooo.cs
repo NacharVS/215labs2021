69ae4ce1c3120816c42af6a15ca52a2e64e595eb
zarina666 <Sharafieva-zarina@mail.ru>
2021-12-21 15:48:44 +0300

    infoooo

---
+++ Labs215Y2K/infooo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    class Info : Charactergame
+    {
+
+        internal static void startredactor(List<Archer> archer, List<Warrior> warrior, List<Mage> mag, double newmaxhealth)
+        {
+            Console.WriteLine("Добро пожаловать в редактор персонажей! Что вы хотите сделать?");
+            Console.WriteLine("1 - Просмотреть характеристики мага");
+            Console.WriteLine("2 - Просмотреть характеристики воина");
+            Console.WriteLine("3 - Просмотреть характеристики лучника");
+            int answer = int.Parse(Console.ReadLine());
+            switch (answer)
+            {
+                case 1:
+                    Mage.startmage(archer, warrior, mag, newmaxhealth);
+                    break;
+                case 2:
+                    Warrior.startwarrior(archer, warrior, mag, newmaxhealth);
+                    break;
+                case 3:
+                    Archer.startarcher(archer, warrior, mag, newmaxhealth);
+                    break;
+                default:
+                    Console.WriteLine("Неверное число!");
+                    Info.startredactor(archer, warrior, mag, newmaxhealth);
+                    break;
+            }
+        }
+    }
+}
