ffb0eb961185d8a1f544aa570df2645e01730968
zarina666 <Sharafieva-zarina@mail.ru>
2021-12-21 15:33:48 +0300

    uuuu

---
+++ Labs215Y2K/Account.cs
@@ -0,0 +1,16 @@
+﻿namespace Labs215Y2K
+{
+    internal class Account
+    {
+        private string v1;
+        private int v2;
+
+        public Account(string v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public int BalanceChangeEvent { get; internal set; }
+    }
+}
---
+++ Labs215Y2K/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K
+{
+    public partial class Game
+    {
+        public Game()
+        {
+            static void Main(String[] arges)
+            {
+
+
+
+            }
+        }
+    }
+}
+        
