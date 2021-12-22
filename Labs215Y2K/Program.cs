ef2ae76f18aac85ffc46f8adb72d3a751d066275
Gazhos215 <9253924@mail.ru>
2021-12-22 17:09:48 +0300

    test

--- Labs215Y2K/Program.cs
+++ Labs215Y2K/Program.cs
@@ -29,8 +29,6 @@ namespace Labs215Y2K
             //Console.WriteLine();
             //Console.WriteLine(a);
 
-
-
         }
         public static void magaz(int[] numberofproduct, int yesno, string[] catalog, string[] korzina, int counter, int cycleoperation, int[] countofproduct, int[] korzinakolvo, int[] price, int[] Sum, int[] sumtwo, int[] korzinaprice)
         {
