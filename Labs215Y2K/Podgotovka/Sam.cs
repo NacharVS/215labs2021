100edb78bd24c82acc2d8b8e8d1e605aaeac7a31
bulatboba <safikovbulan6@gmail.com>
2021-12-23 17:10:38 +0300

    podg2

--- Labs215Y2K/Podgotovka/Sam.cs
+++ Labs215Y2K/Podgotovka/Sam.cs
@@ -216,6 +216,36 @@ namespace Labs215Y2K.Podgotovka
             //}
             //Console.WriteLine();
             //Console.WriteLine($"{sum}");
+
+
+
+            //int[,] array = new int[5, 5]; 
+            //for( int i =0; i < array.GetLength(0); i++)
+            //{
+            //    for(int j=0; j < array.GetLength(1);j++)
+            //    {
+
+            //        array[i, j] = new Random().Next(1, 10);
+            //        Console.Write($" {array[i,j]} ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //Console.WriteLine("Введите любой символ");
+            //static void Mama()
+            //{
+            //    char a= char.Parse(Console.ReadLine());
+            //    Console.WriteLine($" {a} ");
+            //}
+            //Console.WriteLine();
+            //Mama();
+            //Mama();
+            //Mama();
+            //Mama();
+            //Mama();
+            //Mama();
+
         }
 
     }
