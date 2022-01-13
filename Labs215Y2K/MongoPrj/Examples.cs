ee4beac69de383f4752fb833c7af03a1f96da739
marcelomarv <kuzyakovmarsel@gmail.com>
2022-01-13 15:09:00 +0300

    saf

---
+++ Labs215Y2K/MongoPrj/Examples.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K.MongoPrj
+{
+    class Examples
+    {
+        public static void AddToDataBase(string name, int age)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("PetrovBigOriginal");
+            var collection = database.GetCollection<Person>("Persons");
+            collection.InsertOne(new Person(name, age));
+        }
+    }
+}
---
+++ Labs215Y2K/MongoPrj/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs215Y2K.MongoPrj
+{
+    class Student
+    {
+        private string _name;
+        private string _surname;
+        private string _otchestvo;
+        private int _age;
+
+        public Student(string name, string surname, string otchestvo, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Otchestvo = otchestvo;
+            Age = age;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public string Surname { get => _surname; set => _surname = value; }
+        public string Otchestvo { get => _otchestvo; set => _otchestvo = value; }
+        public int Age { get => _age; set => _age = value; }
+    }
+}
