3c245f60434fd757d3727edb5888647454e15ee3
nikita <zixerok22@gmail.com>
2022-01-13 19:42:51 +0300

    test

---
+++ ForDataBase/Data Base/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Person
+{
+   
+        public string name;
+        public int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+   
+}
