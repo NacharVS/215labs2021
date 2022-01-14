949f3e29ea25769e23ab5f76209dcf8909dbb0f4
marcelomarv <kuzyakovmarsel@gmail.com>
2022-01-14 13:19:42 +0300

    db

--- Labs215Y2K/MongoPrj/Examples.cs
+++ Labs215Y2K/MongoPrj/Examples.cs
@@ -7,12 +7,13 @@ namespace Labs215Y2K.MongoPrj
 {
     class Examples
     {
-        public static void AddToDataBase(string name, string surname, string otchestvo, int age)
+        public static void AddToDataBase(string name, string surname, string otchestvo, int age, string gender, int year)
         {
             var client = new MongoClient("mongodb://localhost");
             var database = client.GetDatabase("StudentsDateBaseInfo");
             var collection = database.GetCollection<Student>("Students");
-            collection.InsertOne(new Student(name, surname, otchestvo, age));
+            collection.InsertOne(new Student(name, surname, otchestvo, age, gender, year));
+           
         }
         public static void Start()
         {
@@ -28,7 +29,64 @@ namespace Labs215Y2K.MongoPrj
                 string otchestvo = Console.ReadLine();
                 Console.Write("Введите возраст студента: ");
                 int age = int.Parse(Console.ReadLine());
-                Examples.AddToDataBase(name, surname, otchestvo, age);
+                if (age < 16 || age > 25)
+                {
+                    while ( age < 16 || age > 25)
+                    {
+                        Console.Write("Это не возраст студента!Введите возраст по новому: ");
+                        age = int.Parse(Console.ReadLine());
+                        if (age < 16 || age > 25)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                else if (age >= 16 || age <= 25)
+                {
+
+                }
+                Console.Write("Введите пол студента: 1 - М; 2 - Ж:  " ) ;
+                int question = int.Parse(Console.ReadLine());
+                string gender = "";
+                while (question != 1 || question != 2)
+                {
+                    Console.Write("Неверный пол!Введите цифру студента заново: ");
+                    question = int.Parse(Console.ReadLine());
+                    if (question == 1 )
+                    {
+                        gender = "М";
+                        break;
+                    }
+                    else if (question == 2)
+                    {
+                        gender = "Ж";
+                        break;
+                    }
+                    else if (question != 1 || question != 2)
+                    {
+                        continue;
+                    }
+                }
+                Console.WriteLine("Введите год поступления студента: ");
+                int year = int.Parse(Console.ReadLine());
+                while (year < 2018 || year > 2022)
+                {
+                    Console.Write("Неверный год!Введите год поступления студента заново: ");
+                    year = int.Parse(Console.ReadLine());
+                    if (year <= 2018 || year >= 2022)
+                    {
+                        continue;
+                    }
+                    else if (year < 2018 || year > 2022)
+                    {
+                        break;
+                    }
+                }
+                Examples.AddToDataBase(name, surname, otchestvo, age, gender, year);
                 Console.WriteLine("Хотите продолжить? 1 - Да, 2 - Нет");
                 int answer = int.Parse(Console.ReadLine());
                 Console.WriteLine();
--- Labs215Y2K/MongoPrj/Student.cs
+++ Labs215Y2K/MongoPrj/Student.cs
@@ -10,18 +10,24 @@ namespace Labs215Y2K.MongoPrj
         private string _surname;
         private string _otchestvo;
         private int _age;
+        private string _gender;
+        private int _year;
 
-        public Student(string name, string surname, string otchestvo, int age)
+        public Student(string name, string surname, string otchestvo, int age, string gender, int year)
         {
             Name = name;
             Surname = surname;
             Otchestvo = otchestvo;
             Age = age;
+            Gender = gender;
+            Year = year;
         }
 
         public string Name { get => _name; set => _name = value; }
         public string Surname { get => _surname; set => _surname = value; }
         public string Otchestvo { get => _otchestvo; set => _otchestvo = value; }
         public int Age { get => _age; set => _age = value; }
+        public string Gender { get => _gender; set => _gender = value; }
+        public int Year { get => _year; set => _year = value; }
     }
 }
