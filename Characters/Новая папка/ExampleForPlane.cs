c97e84a4d72973fd22b62f535f5c611f11500cc1
marcelomrv <kuzyakovmarsel@gmail.com>
2022-01-20 11:46:41 +0300

    project

---
+++ Characters/Новая папка/ExampleForPlane.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Driver;
+
+namespace Characters.Новая_папка
+{
+    internal class ExampleForPlane
+    {
+        //public static void a()
+        //{
+        //    console.writeline($"{datetime.now.hour}:{datetime.now.minute}");
+        //}
+
+        private string _numberofflight;
+        private 
+    }
+}
