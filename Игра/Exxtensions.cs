4825cedc4ae03cc1fb19a78b4840199cddf3c851
DanilTasevcev215 <tasevcevdanil18@gmail.com>
2022-01-22 11:58:30 +0300

    Day 14

---
+++ Игра/Exxtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Driver;
+
+namespace Игра.MongoExamples
+{
+    static class Extensions
+    {
+        public static void AddToDatabase(string name, string surname, string gender, string othestvo, int ageHappy, int numbersGroup, int averageScore, bool commerce, int payment, string activity)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("MckKtitc");
+            var collection = database.GetCollection<Person>("Peson");
+            collection.InsertOne(new Person(name, surname, gender, othestvo, ageHappy, numbersGroup, averageScore, commerce, payment, activity));
+        }
+        public static void GetListDataBase()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("MckKtitc");
+            var collection = database.GetCollection<Person>("Peson");
+            List<Person> list = collection.Find(x => true).ToList();
+            foreach (var item in list)
+            {
+                Console.WriteLine($"имя:{item._name}, фамилия:{item._surname}, отчество:{item._othestvo}, пол:{item._gender}, год рождения:{ item._ageHappy}, номер группы:{ item._numbersGroup}, активности:{ item._activity}, комерция:{item._commerce} ,плата:{item._payment}");
+            }
+        }
+        public static void GetFindScoreDataBase()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Введите оценку");
+            int Score = int.Parse(Console.ReadLine());
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("MckKtitc");
+            var collection = database.GetCollection<Person>("Peson");
+            List<Person> list = collection.Find(x => x._averageScore >= Score).ToList();
+            foreach (var item in list)
+            {
+                Console.WriteLine($" имя:{item._name}, фамилия:{item._surname}, оценка:{item._averageScore}, актовности {item._activity}");
+            }
+        }
+        public static void GetFindCommerceDataBase()
+        {
+            Console.WriteLine();
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("MckKtitc");
+            var collection = database.GetCollection<Person>("Peson");
+            List<Person> list = collection.Find(x => x._commerce == true).ToList();
+            foreach (var item in list)
+            {
+                Console.WriteLine($" имя:{item._name}, фамилия:{item._surname}, комерция:{item._commerce} ,плата:{item._payment}");
+            }
+        }
+        public static void GetFindActivnostDataBase()
+        {
+            Console.WriteLine();
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("MckKtitc");
+            var collection = database.GetCollection<Person>("Peson");
+            List<Person> list = collection.Find(x => x._activity != "").ToList();
+            foreach (var item in list)
+            {
+                Console.WriteLine($" имя:{item._name}, фамилия:{item._surname}, активности {item._activity}");
+            }
+        }
+
+    }
+}
---
+++ Игра/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+
+namespace Игра
+{
+   class Person
+   {
+        public ObjectId Id { get; set; }
+        public string _name;
+        public string _surname;
+        public string _othestvo;
+        public string _gender;
+        public int _ageHappy;
+        public int _numbersGroup;
+        public int _averageScore;
+        public bool _commerce;
+        public int _payment;
+        public string _activity;
+
+
+        public Person(string name, string surname, string gender, string othestvo, int ageHappy, int numbersGroup, int averageScore, bool commerce, int payment, string activity)
+        {
+            this._name = name;
+            this._surname = surname;
+            this._gender = gender;
+            this._othestvo = othestvo;
+            this._ageHappy = ageHappy;
+            this._numbersGroup = numbersGroup;
+            this._averageScore = averageScore;
+            this._commerce = commerce;
+            this._payment = payment;
+            this._activity = activity;
+        }
+    }
+}
